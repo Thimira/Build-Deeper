9a46cd685651eb78c2d0d81b6c144527ac4bc35b
Thimira Amaratunga <thimiraamara@gmail.com>
2018-12-08 15:10:50 +0530

    cleaning up face_detection_video code

--- Chapter 9/face_detection_video.py
+++ Chapter 9/face_detection_video.py
@@ -2,7 +2,9 @@ import numpy as np
 import cv2
 import dlib
 
+# Create the video capture object for camera id '0'
 video_capture = cv2.VideoCapture(0)
+# Load the buil-in face dedector of Dlib 
 detector = dlib.get_frontal_face_detector()
 
 while True:
@@ -10,16 +12,21 @@ while True:
     ret, frame = video_capture.read()
 
     if (ret):
+        # Create a grayscale copy of the captured frame
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+        # Get the detected face bounding boxes, using the grayscale image
         rects = detector(gray, 0)
 
+        # Loop over the bounding boxes, if there are more than one face
         for rect in rects:
+            # Get the OpenCV coordinates from the Dlib rectangle objects
             x = rect.left()
             y = rect.top()
             x1 = rect.right()
             y1 = rect.bottom()
 
+            # Draw a rectangle around the face bounding box in OpenCV
             cv2.rectangle(frame, (x, y), (x1, y1), (0, 0, 255), 2)
 
         # Display the resulting frame
